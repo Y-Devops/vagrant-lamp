f7ecf4e51ed83179c297965fe3de72758f732119
Leonardo Luduena <lleo@linux.com>
2019-11-19 21:04:08 +0000

    updating php installation to bionic release

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+export DEBIAN_FRONTEND=noninteractive
+
 # Update Packages
 apt-get update
 # Upgrade Packages
@@ -13,8 +15,8 @@ apt-get install -y apache2
 a2enmod rewrite
 
 #Add Onrej PPA Repo
-apt-add-repository ppa:ondrej/php
-apt-get update
+#apt-add-repository ppa:ondrej/php
+#apt-get update
 
 # Install PHP
 apt-get install -y php7.2
@@ -27,8 +29,13 @@ service apache2 restart
 
 # PHP Mods
 apt-get install -y php7.2-common
-apt-get install -y php7.2-mcrypt
+#apt-get install -y php7.2-mcrypt
 apt-get install -y php7.2-zip
+
+apt-get install -y php-pear php7.2-dev libmcrypt-dev gcc make autoconf libc-dev pkg-config
+pecl -q install mcrypt-1.0.1
+echo 'extension=mcrypt.so' | sudo tee -a /etc/php/7.2/mods-available/mcrypt.ini
+ln -s /etc/php/7.2/mods-available/mcrypt.ini /etc/php/7.2/cli/conf.d/20-mcrypt.ini
 
 # Set MySQL Pass
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
